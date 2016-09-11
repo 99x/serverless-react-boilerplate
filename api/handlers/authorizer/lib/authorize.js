e48bed2409a0b6b3b6041584d7c1e54a7779f002
Manoj Fernando <manojf@99x.lk>
2016-09-12 00:06:05 +0530

    Backend functionalities.

--- api/handlers/authorizer/lib/authorize.js
+++ api/handlers/authorizer/lib/authorize.js
@@ -4,6 +4,7 @@ const policy = require('./policy.json');
 
 module.exports.validate = (context, event) => {
     context.succeed({
+        "principalId": "manoj",
         "policyDocument": policy
     });
 };
