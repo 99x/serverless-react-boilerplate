4714184791a695b5c9078a5b0e5c1fd324a90915
Manoj Fernando <manojf@99x.lk>
2016-10-03 19:21:51 +0530

    Added build automation with gulp and webpack.

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,58 @@
 var gulp = require('gulp'),
     isWin = /^win/.test(process.platform),
     commandSeparator = isWin ? '&' : ';',
-    webpack = require('webpack');
+    webpack = require('webpack'),
+    exec = require('child_process').exec,
+    gulpSequence = require('gulp-sequence');
 
-function runCommand(cmd) {
-    var exec = require('child_process').exec,
-        ls = exec(cmd);
+function runCommand(cmd, done) {
+    var ls = exec(cmd);
     ls.stdout.on('data', function(data) {
         console.log(data);
     });
     ls.stderr.on('data', function(data) {
         console.log(data);
     });
+    ls.on('close', function(data) {
+        done();
+    });
 }
 
-/* Start dynamodb local instancecls */
+/* Install dynamodb locally */
+gulp.task('install-dynamodb', function(done) {
+    console.log('This will take about 1 minute to complete at first run');
+    runCommand('cd serverless' + commandSeparator + ' sls dynamodb install', done);
+});
+
+/* Start dynamodb local instance */
 gulp.task('start-dynamodb', function(done) {
-    runCommand('cd serverless' + commandSeparator + ' sls dynamodb start');
+    runCommand('cd serverless' + commandSeparator + ' sls dynamodb start', done);
 });
 
 /* Start offline server for local development */
 gulp.task('start-offline-server', function(done) {
-    runCommand('cd serverless ' + commandSeparator + 'serverless offline start');
+    runCommand('cd serverless ' + commandSeparator + 'serverless offline start', done);
 });
 
+/* Start offline server for local development */
 gulp.task('start-client', function(done) {
-    runCommand('cd web ' + commandSeparator + ' webpack-dev-server');
+    runCommand('cd web ' + commandSeparator + 'webpack-dev-server', done);
 });
 
-gulp.task('app', ['start-dynamodb', 'start-offline-server', 'start-client']);
+/* Start offline server for local development */
+gulp.task('open-website', function(done) {
+    require("opn")("http://localhost:8080");
+});
+
+/* Remove dynamodb local instance */
+gulp.task('uninstall-dynamodb', function(done) {
+    runCommand('cd serverless' + commandSeparator + ' sls dynamodb remove', done);
+});
+
+/* Deploy service in AWS */
+gulp.task('deploy', ['uninstall-dynamodb'], function() {
+    runCommand('cd serverless' + commandSeparator + ' sls deploy');
+});
+
+/* Start application locally */
+gulp.task('default', gulpSequence('install-dynamodb', ['start-client', 'open-website', 'start-dynamodb', 'start-offline-server']));
