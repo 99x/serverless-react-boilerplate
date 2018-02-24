434e28f7d0131009d593d1d14858ab3904018515
Manoj Fernando <mj.bitwave@gmail.com>
2018-02-25 02:13:16 +0530

    update package.json

--- web/src/index.js
+++ web/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import Foundation from 'foundation-sites/dist/foundation.min.css';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
