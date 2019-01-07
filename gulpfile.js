b0bb6ffc30cefc24ed85d5618c3970e1cef8df0d
TRomesh <tharakaromesh@gmail.com>
2019-01-07 22:00:02 +0530

    update serverless and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,49 @@
-var gulp = require('gulp'),
+let gulp = require("gulp"),
   isWin = /^win/.test(process.platform),
-  commandSeparator = isWin ? '&' : ';',
-  webpack = require('webpack'),
-  exec = require('child_process').exec,
-  util = require('gulp-util'),
-  gulpSequence = require('gulp-sequence'),
+  commandSeparator = isWin ? "&" : ";",
+  webpack = require("webpack"),
+  exec = require("child_process").exec,
   stage = null;
 
 function runCommand(cmd, done) {
-  var ls = exec(cmd);
-  ls.stdout.on('data', function (data) {
+  let ls = exec(cmd);
+  ls.stdout.on("data", function(data) {
     console.log(data);
   });
-  ls.stderr.on('data', function (data) {
+  ls.stderr.on("data", function(data) {
     console.log(data);
   });
-  ls.on('close', function (data) {
+  ls.on("close", function(data) {
     done && done();
   });
 }
 
 /* Install dynamodb local instance */
-gulp.task('install-dynamodb', function (done) {
-  runCommand('cd api' + commandSeparator + ' sls dynamodb install', done);
+gulp.task("install-dynamodb", function(done) {
+  runCommand("cd api" + commandSeparator + " sls dynamodb install", done);
 });
 
 /* Start dynamodb local instance */
-gulp.task('start-dynamodb', function (done) {
-  runCommand('cd api' + commandSeparator + ' sls dynamodb start', done);
+gulp.task("start-dynamodb", function(done) {
+  runCommand("cd api" + commandSeparator + " sls dynamodb start", done);
 });
 
 /* Start offline server for local development */
-gulp.task('start-offline-server', function (done) {
-  runCommand('cd api ' + commandSeparator + 'cd todo ' + commandSeparator + 'serverless offline start', done);
+gulp.task("start-offline-server", function(done) {
+  runCommand(
+    "cd api " +
+      commandSeparator +
+      "cd todo " +
+      commandSeparator +
+      "serverless offline start",
+    done
+  );
 });
 
 /* Start offline server for local development */
-gulp.task('start-client', function (done) {
-  runCommand('cd web ' + commandSeparator + 'npm run start', done);
+gulp.task("start-client", function(done) {
+  runCommand("cd web " + commandSeparator + "npm run start", done);
 });
 
 /* Start application locally */
-gulp.task('serve', gulpSequence(['start-offline-server', 'start-client']));
+gulp.task("serve", gulp.parallel("start-offline-server", "start-client"));
